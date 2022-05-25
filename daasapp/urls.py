2953f1f37dec82591cf85934d1fa3f4048c82009
mickeybs <mickeysimanemb@gmail.com>
2022-05-25 15:27:47 +0300

    predict sowing and harvesting

--- daasapp/urls.py
+++ daasapp/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("Homepage", views.Homepage, name="index"),
+    path("Homepage", views.Homepage, name="Homepage"),
     path("generate", views.generate, name="generate"),
     path("chat", views.chat, name="chat"),
-    path("<str:sowing>/<str:harvesting>/report", views.report, name="report"),
+    path("<str:sowing>/<str:harvesting>/<str:district>/report", views.report, name="report"),
     path("login", views.login, name="login"),
     path("signup", views.signup, name="signup"),
     path("contact", views.contact, name="contact"),
--- daasapp/views.py
+++ daasapp/views.py
@@ -6,13 +6,9 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 import datetime
 # Create your views here.
-
-def generate(request):
-    date = datetime.datetime.now()
-    districts = requests.get("http://127.0.0.1:8080/dbapi/district")
-    ds = W
-    # tmin = requests.get("http://127.0.0.1:8080/dbapi/weather").json
-    # tmax = requests.get("http://127.0.0.1:8080/dbapi/weather").json
+date = datetime.datetime.now()
+def generate(request):    
+    districts = requests.get("http://127.0.0.1:8080/dbapi/district") 
     value = {
         "year": date.year,
         "month": date.month,
@@ -27,33 +23,30 @@ def generate(request):
         "tmax": 23.3
     }
     if request.method == 'POST':
-        district = request.POST['District']
+        district = request.POST['District']        
         print(district)
         response = requests.post("http://127.0.0.1:8080/api/" + district + '/', data=value)
         harvest_response = requests.post("http://127.0.0.1:8080/api/harvest_" + district + '/', data=value2)
         print(response.text, harvest_response.text)
         
-        return redirect(report, sowing=response.text, harvesting=harvest_response.text)
-
+        return redirect(report, sowing=response.text, harvesting=harvest_response.text, district=district)
 
     return render(request, "index.html", {
         "districts": districts.json
     })
 
 def Homepage(request):
-    value = {
-        "year": 2023,
-        "month": 7,
-        "day": 3,
-        "tmax": 18.4
-    }
+    # value = {
+    #     "year": 2023,
+    #     "month": 7,
+    #     "day": 3,
+    #     "tmax": 18.4
+    # }
     
-    response = requests.post("http://127.0.0.1:8080/api/rain/", data=value)
-    print(response.status_code)
+    # response = requests.post("http://127.0.0.1:8080/api/rain/", data=value)
+    # print(response.status_code)
     # return HttpResponseRedirect(reverse("report", args=(response.content)))
-    return render(request, "homepage.html", {
-        "rain": response.text
-    })    
+    return render(request, "homepage.html")    
 
 def chat(request):
     return render(request, "chat.html")
@@ -64,28 +57,25 @@ def contact(request):
 def login(request):
     return render(request, "login.html")
 
-def report(request, sowing, harvesting):
-    print(sowing, harvesting)
-    if sowing == '[true]' and harvesting == "[true]":
-        return render(request, "report.html", {
-            "sowing": "True",
-            "harvesting": "True"
-        }) 
-    elif sowing == "[false]" and harvesting == "[true]":
-        return render(request, "report.html", {
-            "sowing": "False",
-            "harvesting": "True"
-        }) 
-    elif sowing == "[true]" and harvesting == "[false]":
-        return render(request, "report.html", {
-            "sowing": "True",
-            "harvesting": "False"
-        }) 
-    elif sowing == "[false]" and harvesting == "[false]":
-        return render(request, "report.html", {
-            "sowing": "False",
-            "harvesting": "False"
-        }) 
+def report(request, sowing, harvesting, district):
+    tmean = float(requests.get(f"http://127.0.0.1:8080/dbapi/weatherdetail/{date.year}/{date.month}/{date.day}/{district}").text)
+    print(sowing, harvesting, tmean)  
+    sow = None
+    harvest = None
+    if sowing == '[true]' and (tmean >= 15 and tmean <= 20):
+        sow = True  
+    else:
+        sow = False  
+    print(sow)
+    if harvesting == '[true]' and tmean > 20:
+        harvest = True
+    else:
+        harvest = False
+    print(harvest)
+    return render(request, "report.html", {
+            "sowing": sow,
+            "harvesting": harvest
+        })   
 
 def signup(request):
     return render(request, "signup.html")
